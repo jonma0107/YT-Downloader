9909e7d19ae994815d5fddcecf631437e9eca0d0
jonma0107 <jduplantier44@gmail.com>
2023-08-19 22:14:44 -0500

    [ADD] files for deploy

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://yt-downloader.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://yt-downloader.up.railway.app']
 
 
 # Application definition
